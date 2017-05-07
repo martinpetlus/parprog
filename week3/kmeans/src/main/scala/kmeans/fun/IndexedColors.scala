a6581363e2ee3ca675a3064045f721752b178fa0
Martin Petluš <martinpetlus@gmail.com>
2017-05-07 13:07:10 +0200

    Use parallel collections

--- week3/kmeans/src/main/scala/kmeans/fun/IndexedColors.scala
+++ week3/kmeans/src/main/scala/kmeans/fun/IndexedColors.scala
@@ -22,11 +22,11 @@ class IndexedColorFilter(initialImage: Img,
   private var steps = 0
 
   // What could we do here to speed up the computation?
-  val points = imageToPoints(initialImage)
-  val means = initializeIndex(colorCount, points)
+  val points = imageToPoints(initialImage).par
+  val means = initializeIndex(colorCount, points).par
 
   /* The work is done here: */
-  private val newMeans = kMeans(points, means, 0.01)
+  private val newMeans = kMeans(points, means, 0.01).par
 
   /* And these are the results exposed */
   def getStatus() = s"Converged after $steps steps."
