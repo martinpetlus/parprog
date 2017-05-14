25b47d65fdd06e98987655e24969e469e9a0bfb2
Martin Petluš <martinpetlus@gmail.com>
2017-05-14 19:18:50 +0200

    Implement updateBodies

--- week4/barneshut/src/main/scala/barneshut/Simulator.scala
+++ week4/barneshut/src/main/scala/barneshut/Simulator.scala
@@ -47,7 +47,7 @@ class Simulator(val taskSupport: TaskSupport, val timeStats: TimeStatistics) {
   def updateBodies(bodies: Seq[Body], quad: Quad): Seq[Body] = timeStats.timed("update") {
     val parBodies = bodies.par
     parBodies.tasksupport = taskSupport
-    ???
+    bodies.aggregate(Seq[Body]())(_ :+ _.updated(quad), _ ++ _)
   }
 
   def eliminateOutliers(bodies: Seq[Body], sectorMatrix: SectorMatrix, quad: Quad): Seq[Body] = timeStats.timed("eliminate") {
